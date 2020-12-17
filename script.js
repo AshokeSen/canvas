9695bda54fe0805c51c1af8fec53c30ae0ba2b0f
AshokeSen <souvikpythonista@gmail.com>
2020-12-17 20:04:20 +0530

    Changing bg images to asset images

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector(".container");
 
 const pictures = [
-  "url('https://images.unsplash.com/photo-1560719268-60a714bd4c31?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80')",
-  "url('https://images.unsplash.com/photo-1543237935-aa9ffc1c0bc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')",
-  "url('https://images.unsplash.com/photo-1522410818928-5522dacd5066?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')",
-  "url('https://images.unsplash.com/photo-1563784269116-ff83fae17724?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1188&q=80')",
+  "url('./assets/canvas1.jpg')",
+  "url('./assets/canvas2.jpg')",
+  "url('./assets/canvas3.jpg')",
+  "url('./assets/canvas4.jpg')",
 ];
 
 let index = 1;
